b162499412211bc20ce501eb9c851acef72c4666
deepak <deepak.kadarivel@gmail.com>
2016-09-18 11:19:25 +0530

    Tablayout added for viewpager

--- app/src/main/java/com/upbeat/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/upbeat/smellslikebakin/ViewPagerFragment.java
@@ -2,22 +2,22 @@ package com.upbeat.smellslikebakin;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class ViewPagerFragment extends Fragment {
-    public static final String KEY_RECIPE_INDEX =   "key_recipe_index";
+    public static final String KEY_RECIPE_INDEX = "key_recipe_index";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         int index = getArguments().getInt(KEY_RECIPE_INDEX);
-        View view = inflater.inflate(R.layout.fragment_view,container, false);
+        View view = inflater.inflate(R.layout.fragment_view, container, false);
         getActivity().setTitle(Recipes.names[index]);
 
         final IngredientsFragment ingredientsFragment = new IngredientsFragment();
@@ -34,7 +34,15 @@ public class ViewPagerFragment extends Fragment {
             public int getCount() {
                 return 2;
             }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
         });
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
         return view;
     }
 
