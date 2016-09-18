26de5022246c7cd363f943ddb9f45fba6010ceb4
deepak <deepak.kadarivel@gmail.com>
2016-09-18 10:00:54 +0530

    Change from app Fragment to support fragment

--- app/src/main/java/com/upbeat/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/upbeat/smellslikebakin/MainActivity.java
@@ -1,7 +1,7 @@
 package com.upbeat.smellslikebakin;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onRe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
         if (savedFragment == null) {
             ListFragment listFragment = new ListFragment();
-            FragmentManager fragmentManager = getFragmentManager();
+            FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.add(R.id.placeHolder, listFragment);
             fragmentTransaction.commit();
@@ -26,5 +26,11 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onRe
     @Override
     public void onListRecipeSelected(int index) {
         Toast.makeText(MainActivity.this, Recipes.names[index], Toast.LENGTH_SHORT).show();
+        ViewPagerFragment viewPagerFragment = new ViewPagerFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.placeHolder, viewPagerFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
